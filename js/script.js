3c087be5aaaf1a05adfe5740f9b16d8a7a24a608
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-11-02 02:22:44 +0600

    gallery about added

--- js/script.js
+++ js/script.js
@@ -30,3 +30,22 @@ var swiper = new Swiper(".home-slider", {
         disableOnInteraction: false,
     }
 });
+
+// 
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    console.log('Hello');
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
